4919508644bb5cf6bcc32c763c5e7f21a163d52f
Justin Lew <justinlew66@gmail.com>
2026-01-18 02:08:47 +1100

    Colour palette of home page

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,6 +1,6 @@
-export default function Navbar () {
+export default function NavBar() {
     return (
-        <div className="w-full h-16 bg-blue-600 text-white flex items-center px-8">
+        <div className="bg-blue-500/10 backdrop-blur-md border-b border-blue-500/50 w-full h-16 border-b border-blue-500 text-white flex items-center px-8">
             <h1 className="text-xl font-bold">Dailies</h1>
         </div>
     );
