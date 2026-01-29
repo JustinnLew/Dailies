b17debd9d3c13ccbec8dca8963cda1481272f36a
Justin Lew <justinlew66@gmail.com>
2026-01-29 21:31:05 +1100

    Strict mode'

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,4 +1,4 @@
-// import { StrictMode } from "react";
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
@@ -7,12 +7,12 @@ import { StyledEngineProvider } from "@mui/material/styles";
 import GlobalStyles from "@mui/material/GlobalStyles";
 
 createRoot(document.getElementById("root")!).render(
-  // <StrictMode>
+  <StrictMode>
     <StyledEngineProvider enableCssLayer>
       <GlobalStyles styles="@layer theme, base, mui, components, utilities;" />
       <BrowserRouter>
         <App />
       </BrowserRouter>
     </StyledEngineProvider>
-  // </StrictMode>,
+  </StrictMode>,
 );
