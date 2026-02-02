96543ff04a3112f674aa722587e56813316f0e99
Justin Lew <justinlew66@gmail.com>
2026-02-02 14:09:40 +1100

    Removed some info! that inflated logs

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -47,7 +47,6 @@ async fn main() {
         let mut interval = interval(Duration::from_secs(60));
         loop {
             interval.tick().await;
-            info!("Running Game Scan");
             // WILL NEED TO BE CHANGED FOR FUTURE GAMES
             // PROBABLY USE THE REGISTRY TO MAP FROM ID TO GAME THEN RETAIN
             scan_state
