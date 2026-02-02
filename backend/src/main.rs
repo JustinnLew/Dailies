f55635c3fc4cdd1c5df5c3b9153be3ba4fd52cf2
justin <z5424659@ad.unsw.edu.au>
2026-02-02 12:18:42 +1100

    Cors

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -58,15 +58,6 @@ async fn main() {
         }
     });
 
-    let cors = CorsLayer::new()
-        .allow_methods([Method::GET, Method::POST])
-        .allow_origin(
-            env::var("FRONTEND_URL")
-                .expect("env.FRONTEND_URL not set")
-                .parse::<HeaderValue>()
-                .unwrap(),
-        );
-
     let app = Router::new()
         .route(
             "/api/guess-the-song/create-lobby",
